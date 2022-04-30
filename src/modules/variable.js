6a05eb5f48b619add78bb6df99735e474ccb2236
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-04-30 17:29:45 +0300

    Create modules

---
+++ src/modules/variable.js
@@ -0,0 +1,6 @@
+
+const taskContainer = document.querySelector(".task-container");
+const taskInput = document.querySelector(".task-input");
+const clearComplete = document.querySelector(".clear-completed");
+
+export {taskContainer, taskInput, clearComplete}
