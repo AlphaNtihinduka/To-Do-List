d118ecb719db492883793de6d05672dbb405194b
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-05-06 02:17:00 +0300

    update index

--- src/modules/complete.js
+++ src/modules/complete.js
@@ -2,7 +2,11 @@ const clearCompleted = document.querySelector('.clear-completed');
 clearCompleted.addEventListener('click', () => {
   const storage = JSON.parse(localStorage.getItem('tasks'));
   const notCompleted = storage.filter((item) => item.completed === false);
+  for (let i = 0; i < notCompleted.length; i += 1) {
+    notCompleted[i].index = i + 1;
+  }
   localStorage.setItem('tasks', JSON.stringify(notCompleted));
+
   window.location.reload();
 });
 
