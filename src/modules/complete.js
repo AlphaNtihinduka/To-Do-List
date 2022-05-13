012bd7522366e1500fa03e83055199d33b55b43d
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-05-14 02:51:49 +0300

    modify index and complete

--- src/modules/complete.js
+++ src/modules/complete.js
@@ -1,6 +1,6 @@
-const clearCompleted = document.querySelector('.clear-completed');
-clearCompleted.addEventListener('click', () => {
-  const storage = JSON.parse(localStorage.getItem('tasks'));
+
+const clearCompleted = () => {
+    const storage = JSON.parse(localStorage.getItem('tasks'));
   const notCompleted = storage.filter((item) => item.completed === false);
   for (let i = 0; i < notCompleted.length; i += 1) {
     notCompleted[i].index = i + 1;
@@ -8,6 +8,12 @@ clearCompleted.addEventListener('click', () => {
   localStorage.setItem('tasks', JSON.stringify(notCompleted));
 
   window.location.reload();
-});
+}
 
-export default clearCompleted;
+const clearListener = () => {
+  document.querySelector('.clear-completed').addEventListener('click',clearCompleted)
+}
+clearListener()
+
+
+export default clearListener;
